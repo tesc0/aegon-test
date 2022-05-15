df5aa4387850a8820e3cdf5d6cb9da3ad6529085
tesco <zs0lt.santa@gmail.com>
2022-05-15 20:01:42 +0200

    both job working side by side; previous commit: now unused functions have been removed from languagebatchbo

--- src/generate_language_files.php
+++ src/generate_language_files.php
@@ -5,5 +5,5 @@ chdir(__DIR__);
 include('../vendor/autoload.php');
 
 $languageBatchBo = new \Language\LanguageBatchBo();
-//$languageBatchBo->generateLanguageFiles();
+$languageBatchBo->generateLanguageFiles();
 $languageBatchBo->generateAppletLanguageXmlFiles();
